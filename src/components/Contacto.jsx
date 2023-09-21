81ab23e3b8b825524627458b3c955b8f702ecb8d
gcaudullo <gonchicaudullo@gmail.com>
2023-09-21 09:29:40 -0300

    mejoro pagina contactos y nosotros

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -12,14 +12,43 @@ const Contacto = () => {
   return (
     <Container>
       <h1 className='main-title'>Contacto</h1>
+      <p>
+        ¡Gracias por su interés en OK Computers!
+      </p>
+      <p>Estamos aquí para responder a todas sus preguntas y brindarle la asistencia que necesita.
+        Puede comunicarse con nosotros de varias maneras:
+      </p>
+
+      <h2>Formulario de Contacto</h2>
+      <p>
+        Por favor, utilice el formulario a continuación para ponerse en contacto con nosotros.
+        Complete los campos con su nombre y apellido, dirección de correo electrónico, número de teléfono y dejanos tu mensaje con la consulta.
+        Uno de nuestros representantes se comunicará con usted a la brevedad posible.
+      </p>
       <form className='formulario' onSubmit={handleSubmit(enviar)} >
-        <input type="text" placeholder='Ingresá tu nombre' {...register("nombre")} />
+        <input type="text" placeholder='Ingresá tu nombre y apellido' {...register("nombre")} />
         <input type="email" placeholder='Ingresá tu e-mail' {...register("email")} />
         <input type="phone" placeholder='Ingresá tu telefono' {...register("telefono")} />
         <textarea placeholder='Deja aquí tu mensaje' {...register("mensaje")} />
         <button className='enviar' type="submit">Enviar</button>
       </form>
-    </Container>
+
+      <h2>Información de Contacto</h2>
+      <p><b>Dirección de la Oficina:</b></p>
+      <p>123 Calle Tecnológica</p>
+      <p>Ciudad Tecnoville, TEC123</p>
+
+      <p><b>Teléfono de Atención al Cliente:</b></p>
+      <p>+123 456 7890</p>
+
+      <p><b>Correo Electrónico de Atención al Cliente:</b></p>
+      <p>info@okcomputers.com</p>
+
+      <h2>Horario de Atención</h2>
+      <p>Nuestro equipo de atención al cliente está disponible para ayudarlo de lunes a viernes, de 9:00 a.m. a 6:00 p.m. (hora local).
+        Estamos comprometidos a brindarle respuestas rápidas y soluciones efectivas.
+      </p>
+    </Container >
   )
 }
 
--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container';
+import Carousels from './Carousels';
 
 const Nosotros = () => {
     return (
-    <Container>
-        <h1 className="Title" > Nosotros</h1>
-    </Container>
+        <Container>
+            <h1 className="Title" > Nosotros</h1>
+            <Carousels />
+            <h2>Nuestra Historia</h2>
+            <p>OK Computers nació de una pasión compartida por la tecnología y la visión de proporcionar a nuestros clientes
+                una experiencia incomparable en el mundo de la informática y la electrónica. Fundada en 1995 por un grupo de entusiastas de la tecnología,
+                nuestra empresa ha crecido y evolucionado constantemente a lo largo de los años.</p>
+            <p> Desde nuestros humildes comienzos como una pequeña tienda de computadoras en el centro de la ciudad, hemos ampliado nuestro alcance y
+                diversificado nuestro catálogo de productos. A lo largo de nuestra historia, hemos mantenido firmemente nuestro compromiso
+                con la excelencia en servicio al cliente y la calidad de los productos que ofrecemos.</p>
+
+            <h2>Representando las Mejores Marcas</h2>
+            <p>En OK Computers, nos enorgullece representar algunas de las marcas más confiables y líderes en el mundo de la tecnología.
+                Trabajamos en estrecha colaboración con fabricantes de renombre internacional para ofrecer a nuestros clientes una amplia
+                gama de productos de alta calidad. Algunas de las marcas que representamos incluyen:</p>
+            <p>
+                <ul>
+                    <li><b>TechMega: </b>Líder en laptops y computadoras de última generación.</li>
+                    <li><b>ElectroTech: </b>Especialistas en electrodomésticos de vanguardia para el hogar.</li>
+                    <li><b>MobileWave: </b>Ofreciendo una selección excepcional de teléfonos móviles y accesorios.</li>
+                    <li><b>SoundMaster: </b>Expertos en equipos de audio y sistemas de entretenimiento para el hogar.</li>
+                    <li><b>ComponentesTech: </b>Proveedores de componentes y accesorios esenciales para entusiastas de la computación.</li>
+                    <li><b>GigaNet: </b>Soluciones de redes y conectividad de alta velocidad.</li>
+                </ul>
+                Nuestra dedicación a la calidad, la innovación y el servicio al cliente nos ha permitido ganarnos la confianza de clientes de todo el país
+                a lo largo de los años. Estamos comprometidos a seguir creciendo y brindando soluciones tecnológicas excepcionales a nuestros clientes en el futuro.
+            </p>
+            <p>
+                Gracias por confiar en OK Computers como su destino de confianza para todas sus necesidades tecnológicas.
+                ¡Esperamos poder servirle y ofrecerle productos de calidad excepcional durante muchos años más!
+            </p>
+        </Container>
     )
 }
 
